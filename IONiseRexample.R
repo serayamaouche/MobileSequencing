4119d6ca3175dcd49295a5cff3e5a740b8b1ed43
Seraya Maouche <seraya.maouche@iscb.org>
2017-04-23 12:59:15 +0200

    Update IONiseRexample.R

--- IONiseRexample.R
+++ IONiseRexample.R
@@ -1,3 +1,11 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Script File: IONiseRexample.R
+# Date of creation: 5 Feb 2017
+# Date of last modification: 23 Apr 2017
+# Author: Seraya Maouche <seraya.maouche@iscb.org>
+# Short Description: This script provides functions to QC mobile sequecing data from 
+#                     Oxford Nanopore MinION
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 # Install IONiseR Bioconductor package
@@ -6,6 +14,18 @@ source("https://bioconductor.org/biocLite.R")
 biocLite("IONiseR")
 
 # Load IONiseR and required packages
+library(tibble)
+library(hwriter)
+library(rhdf5)
+library(ShortRead)
 library(IONiseR)
 library(ggplot2)
 library(gridExtra)
+
+
+# Read data
+dataDir <- "M:/Professional/MyCode/MobileSequencing/data"
+fast5files <- list.files(dataDir, pattern = '.fast5$')
+setwd(dataDir)
+example.summary <- readFast5Summary(fast5files)
+str(example.summary)
