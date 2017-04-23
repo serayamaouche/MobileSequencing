1277dc80bca6799e00390ad2c98b7f44b2dc6029
Seraya Maouche <seraya.maouche@iscb.org>
2017-04-23 12:38:16 +0200

    Create IONiseRexample.R

---
+++ IONiseRexample.R
@@ -0,0 +1,11 @@
+
+
+# Install IONiseR Bioconductor package
+# http://bioconductor.org/packages/release/bioc/html/IONiseR.html
+source("https://bioconductor.org/biocLite.R")
+biocLite("IONiseR")
+
+# Load IONiseR and required packages
+library(IONiseR)
+library(ggplot2)
+library(gridExtra)
